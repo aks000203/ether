f68f3c01fc9f79b025c035638a0953553ee9ae26
aks <aks000203@gmail.com>
2019-05-24 01:10:15 +0530

    ..

---
+++ app/src/index.js
@@ -0,0 +1,70 @@
+//import "../src/app.css";
+
+// var Web3 = require('web3');
+
+import Web3 from "web3";
+import votingArtifact from "../../build/contracts/Voting.json";
+
+let candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+
+const App = {
+ web3: null,
+ account: null,
+ voting: null,
+
+ start: async function() {
+  const { web3 } = this;
+
+  try {
+   // get contract instance
+   const networkId = await web3.eth.net.getId();
+   const deployedNetwork = votingArtifact.networks[networkId];
+   this.voting = new web3.eth.Contract(
+    votingArtifact.abi,
+    deployedNetwork.address,
+   );
+
+   // get accounts
+   const accounts = await web3.eth.getAccounts();
+   this.account = accounts[0];
+
+   this.loadCandidatesAndVotes();
+  } catch (error) {
+   console.error("Could not connect to contract or chain.");
+  }
+ },
+
+ loadCandidatesAndVotes: async function() {
+  // The line below loads the totalVotesFor method from the list of methods 
+// returned by this.voting.methods
+  const { totalVotesFor } = this.voting.methods;
+  let candidateNames = Object.keys(candidates);
+  for (var i = 0; i < candidateNames.length; i++) {
+   let name = candidateNames[i];
+   var count = await totalVotesFor(this.web3.utils.asciiToHex(name)).call();
+   $("#" + candidates[name]).html(count);
+  }
+ },
+
+  
+};
+
+window.App = App;
+
+window.addEventListener("load", function() {
+ if (window.ethereum) {
+  // use MetaMask's provider
+  App.web3 = new Web3(window.ethereum);
+  window.ethereum.enable(); // get permission to access accounts
+ } else {
+  console.warn(
+   "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+  );
+  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+  App.web3 = new Web3(
+   new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+  );
+ }
+
+ App.start();
+});
